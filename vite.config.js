38c8978b046183942e0d2eb278954ee74eb6b33e
Pavel Bohovin <pavel.bohovin@gmail.com>
2025-02-13 01:32:03 +0200

    Update icons config

--- vite.config.js
+++ vite.config.js
@@ -22,25 +22,13 @@ export default defineConfig({
             "src": "/manifest-icon-192.maskable.png",
             "sizes": "192x192",
             "type": "image/png",
-            "purpose": "any"
-          },
-          {
-            "src": "/manifest-icon-192.maskable.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
+            "purpose": "any maskable"
           },
           {
             "src": "/manifest-icon-512.maskable.png",
             "sizes": "512x512",
             "type": "image/png",
-            "purpose": "any"
-          },
-          {
-            "src": "/manifest-icon-512.maskable.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
+            "purpose": "any maskable"
           }
         ],
       },
